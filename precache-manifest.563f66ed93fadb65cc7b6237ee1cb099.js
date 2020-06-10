c6a685a5a37833157dd3a5f8ee363f8a929fccee
Junaid Razaq <junaid.razaq@hotmail.com>
2020-06-10 19:57:45 +0100

    Updates

---
+++ precache-manifest.563f66ed93fadb65cc7b6237ee1cb099.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356948c67a9322d67ffe80d786ffdfde",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7e0704e22058605d74de",
+    "url": "/portfolio/static/css/main.ee1598e7.chunk.css"
+  },
+  {
+    "revision": "265e706bcf1c1f636508",
+    "url": "/portfolio/static/js/2.b5a34a82.chunk.js"
+  },
+  {
+    "revision": "93df03793b21857c6e0a75aa70210c51",
+    "url": "/portfolio/static/js/2.b5a34a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0704e22058605d74de",
+    "url": "/portfolio/static/js/main.18dd9cb9.chunk.js"
+  },
+  {
+    "revision": "63ccc55bb54467244bc2",
+    "url": "/portfolio/static/js/runtime-main.f1c7b587.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/portfolio/static/media/avatar.5a021ab9.png"
+  },
+  {
+    "revision": "d2062074e8a8c811f0343d739c2ee673",
+    "url": "/portfolio/static/media/back-img.d2062074.jpg"
+  }
+]);
