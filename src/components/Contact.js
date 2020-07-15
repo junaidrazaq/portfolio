78dc3ef2057c998b578cf7b451e79da87d34df08
Junaid Razaq <junaid.razaq@hotmail.com>
2020-07-15 12:28:31 +0100

    Added Contact page with full functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,42 +1,91 @@
-import React from 'react'
-import { Box, Grid, Typography, Button} from '@material-ui/core'
+import React, { useState } from 'react'
+import { Box, Grid, Typography, Button, TextField} from '@material-ui/core'
 import Navbar from './Navbar';
 import Styles, { InputField } from './Styles'
 import SendIcon from '@material-ui/icons/Send'
+import emailjs from 'emailjs-com'
 
 function Contact() {
     const classes = Styles()
+
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleChange = (event) => {
+        event.target.name=="name" 
+        ? setName(event.target.value)
+        : event.target.name=="email" 
+        ? setEmail(event.target.value)
+        : event.target.name=="message" 
+        ? setMessage(event.target.value)
+        : console.log("error")
+      };
+
+    function sendEmail(e) {
+        e.preventDefault()
+        emailjs.sendForm('gmail', 'junaidtemplate', e.target, 'user_hEXr2KtjDJJrVdy1OxMro')
+      .then((result) => {
+          console.log(result.text);
+          result.text ==="OK" ? console.log("it worked") : alert("didnt work")
+          setName("")
+          setMessage("")
+          setEmail("")
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+
+
     return (
         <Box component='div'>
             <Navbar/>
             <Grid container justify='center'>
-                <Box component='form' className={classes.contactContainer}>
+                <Box component='form' className={classes.contactContainer} onSubmit={sendEmail}>
                     <Typography variant='h5' className={classes.contactHead}>Contact Me</Typography>
                     <InputField
+                    id="name"
+                    name="name"
                     fullWidth={true}
                     label="Name"
                     variant="outlined"
                     margin='dense'
-                    size='medium'/>
+                    size='medium'
+                    onChange={(e) => handleChange(e)}
+                    value={name}
+                    />
                     <br/>
                     <InputField
+                    id="email"
+                    name="email"
                     fullWidth={true}
                     label="Email"
                     variant="outlined"
                     margin='dense'
-                    size='medium'/>
+                    size='medium'
+                    onChange={(e) => handleChange(e)}
+                    value={email}
+                    />
                     <br/>
                     <InputField
+                    id="message"
+                    name="message"
                     fullWidth={true}
-                    label="Company Name"
+                    label="Enter Message Here"
+                    multiline
+                    rows={8}
                     variant="outlined"
                     margin='dense'
-                    size='medium'/>
+                    size='medium'
+                    onChange={(e) => handleChange(e)}
+                    value={message}
+                    />
                     <br/>
-                    <Button 
+                    <Button
+                    type="submit"
                     variant='outlined' 
                     fullWidth={true} 
-                    endIcon={<SendIcon/>} 
+                    endIcon={<SendIcon/>}
                     className={classes.contactButton}>
                         Contact Me
                     </Button>
